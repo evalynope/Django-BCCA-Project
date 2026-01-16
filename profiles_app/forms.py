18fa51a8da5c376c1190c7a6a0bd863eeffc85b9
Your Name <efondren@basecampcodingacademy.org>
2026-01-15 18:10:53 -0600

    latest changes and removed <ul> dots

--- profiles_app/forms.py
+++ profiles_app/forms.py
@@ -4,6 +4,8 @@ from .models import Profile
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from django.contrib.auth.password_validation import validate_password
+
 
 class ProfileForm(ModelForm):
     class Meta:
@@ -32,9 +34,21 @@ class CustomUserCreationForm(UserCreationForm):
         help_text="5–20 characters"
     )
 
+    password1 = forms.CharField(
+        required=True,
+        label="Passwo",
+        widget=forms.PasswordInput(attrs={'placeholder': 'Enter pass'}),
+    )
+
+    password2= forms.CharField(
+        required=True,
+        label="Password",
+        widget=forms.PasswordInput(attrs={'placeholder': 'Conformation password'}),
+    )
+
     class Meta:
         model = User
-        fields = ("username", "password1", "password2")
+        fields = ['email']#("username", "password1", "password2")
 
     def clean_username(self):
         username = self.cleaned_data["username"]
@@ -47,3 +61,12 @@ class CustomUserCreationForm(UserCreationForm):
                 "Username cannot be only numbers."
             )
         return username
+
+    def clean_password1(self):
+        password = self.cleaned_data.get('password1')
+        try:
+            # Run Django's built-in password validators
+            validate_password(password, self.instance)
+        except forms.ValidationError as e:
+            raise forms.ValidationError(e.messages)
+        return password
